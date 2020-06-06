c915b254550a08e119373917c19b2e61174f59f3
Jose Rodriguez <j.rodriguez48@yahoo.com>
2020-06-06 10:08:36 -0500

    working on fixing delete icon

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar'
 import Contact from './components/Contact'
 import { Route, BrowserRouter} from 'react-router-dom'
 import TableList from './components/TableList'
+import DemoTodo from './components/DemoTodo';
 
 
 
@@ -20,7 +21,7 @@ function App() {
       <Route exact path='/' component={About}/>
       <Route path='/Contact' component={Contact}/>
       <Route path='/TableList' component={TableList}/>
-    
+      <Route path='/DemoTodo' component={DemoTodo}/>
     </div>
     </BrowserRouter>
   );
