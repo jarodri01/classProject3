8fea78321d36d6c6a2caec9be447117b81715227
Jose Rodriguez <47047097+jarodri01@users.noreply.github.com>
2020-06-08 23:58:50 -0500

    update files
    delete old files

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ function App() {
       <Route exact path='/' component={About}/>
       <Route path='/Contact' component={Contact}/>
       <Route path='/TableList' component={TableList}/>
-      <Route path='/DemoTodo' component={DemoTodo}/>
     </div>
     </BrowserRouter>
   );
