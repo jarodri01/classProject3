56a7245f8e5f0f2c582a1e8daa43ef703191ef86
Jose Rodriguez <j.rodriguez48@yahoo.com>
2020-06-08 22:20:33 -0500

    update pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,10 +11,12 @@ function Navbar (){
         <Layout>
         <Header className="header">
           <div className="logo" />
+          
             <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
             <Menu.Item key="1"><NavLink exaxt to="/"></NavLink>About</Menu.Item>
-            <Menu.Item key="3"><NavLink to="/Contact"></NavLink>Contact</Menu.Item>
-            <Menu.Item key="5"><NavLink to="/TableList"></NavLink>TableList</Menu.Item>
+            <Menu.Item key="2"><NavLink to="/Contact"></NavLink>Contact</Menu.Item>
+            <Menu.Item key="3"><NavLink to="/TableList"></NavLink>TableList</Menu.Item>
+
           </Menu>
         </Header>
         </Layout>
